bb91d7e41fa7bbd323ae8a90d39d7a584598e84c
Scott Lampert <scott@lampert.org>
2013-03-14 14:03:52 -0700

    First commit.

---
+++ attributes/default.rb
@@ -0,0 +1,8 @@
+default['statsd']['port'] = 8125
+default['statsd']['graphite_port'] = 2003
+default['statsd']['graphite_host'] = "localhost"
+default['statsd']['package_version'] = "0.5.0"
+default['statsd']['sha'] = "712dcfbb588a3babf20acd1947857263b4a0722a"
+default['statsd']['user'] = "statsd"
+default['statsd']['repo'] = "git://github.com/etsy/statsd.git"
+default['statsd']['tmp_dir'] = "/tmp"
