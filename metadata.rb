2c1081415ab81e72165d98d3acefc31e37a634c6
Alan Meadows <alan.meadows@gmail.com>
2013-06-04 13:28:18 -0700

    update readme and metadata for public repo transition

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,16 @@
-maintainer       "Scott Lampert"
-maintainer_email "sl724q@att.com"
+name             "statsd"
+maintainer       "AT&T Services, Inc."
+maintainer_email "cookbooks@lists.tfoundry.com"
 license          "Apache 2.0"
 description      "Installs/Configures statsd"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.2"
+recipe           "statsd", "Installs stats ruby gem"
+recipe           "statsd::server", "Configures statsd server"
 
-depends "build-essential"
-depends "git"
-supports "ubuntu"
+%w{ ubuntu }.each do |os|
+  supports os
+end
 
+depends          "build-essential"
+depends          "git"
