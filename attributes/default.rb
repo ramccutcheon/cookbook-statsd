daf112e5a081041348332fee9d5504b147119429
Ryan Mccutcheon <rx837b@att.com>
2014-06-17 18:06:14 +0000

    rubocop offenses fixed

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,11 @@
+# encoding: UTF-8
+
 default['statsd']['port'] = 8125
 default['statsd']['graphite_port'] = 2003
-default['statsd']['graphite_host'] = "localhost"
+default['statsd']['graphite_host'] = 'localhost'
 default['statsd']['relay_server'] = false
-default['statsd']['package_version'] = "0.6.0"
-default['statsd']['sha'] = "2ccde8266bbe941ac5f79efe39103b99e1196d92"
-default['statsd']['user'] = "statsd"
-default['statsd']['repo'] = "git://github.com/etsy/statsd.git"
-default['statsd']['tmp_dir'] = "/tmp"
+default['statsd']['package_version'] = '0.6.0'
+default['statsd']['sha'] = '2ccde8266bbe941ac5f79efe39103b99e1196d92'
+default['statsd']['user'] = 'statsd'
+default['statsd']['repo'] = 'git://github.com/etsy/statsd.git'
+default['statsd']['tmp_dir'] = '/tmp'
